bcb2c3b4751d66879ad4fdf0229efecc3560cfe0
May <nmmay11@gmail.com>
2021-09-14 09:44:31 +0300

    changes

--- src/components/shared/Tickets.vue
+++ src/components/shared/Tickets.vue
@@ -1,60 +1,68 @@
 <template>
-  <v-container grid-list-xs>
+  <v-container grid-list-lg>
     <v-card>
-      <div class="row">
-        <div class="col" md="2">
-          <input
-            class="filter"
-            v-model="filter"
-            type="search"
-            placeholder="search"
-          />
-        </div>
-      </div>
-      <div class="col">
-        <div class="card shadow">
-          <div class="card-header border-0">
-            <h5 class="mb-0">All Tickets</h5>
-          </div>
-          <div class="table-responsive">
-            <table
-              hover
-              table
-              class="table align-items-center flush"
-              :filter="filter"
-              :per-page="perPage"
-              :current-page="currentPage"
+      <h5 class="header">All Tickets <i class="fas fa-sort-down"></i></h5>
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <input
+          class="search"
+          placeholder="Search here"
+          v-model="filter"
+          :search="search"
+        />
+        <v-btn v-model="filter" class="filter"
+          >Status <i class="fas fa-sort-down"></i
+        ></v-btn>
+      </v-card-actions>
+      <v-divider></v-divider>
+      <table
+        hover
+        table
+        class="table align-items-center flush"
+        :filter="filter"
+        :per-page="perPage"
+        :current-page="currentPage"
+      >
+        <thead class="thead-light">
+          <tr>
+            <th
+              scope="col"
+              data-sortable="true"
+              v-for="(header, index) in ticketTable.tableHeadings"
+              :key="index"
             >
-              <thead class="thead-light">
-                <tr>
-                  <th
-                    scope="col"
-                    data-sortable="true"
-                    v-for="(header, index) in ticketTable.tableHeadings"
-                    :key="index"
-                  >
-                    {{ header }}
-                  </th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr v-for="(ticket, index) in tickets" :key="index">
-                  <td
-                    v-for="(field, fieldIndex) in ticketTable.fields"
-                    :key="fieldIndex"
-                  >
-                    {{ getTicketFieldValues(ticket, field) }}
-                    <view-ticket :tickets="ticket"></view-ticket>
-                  </td>
-                  <td>
-                    <v-chip flat small>{{ ticket.status }}</v-chip>
-                  </td>
-                </tr>
-              </tbody>
-            </table>
-          </div>
-        </div>
-      </div>
+              {{ header }} <i class="fas fa-sort-down"></i>
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="(ticket, index) in tickets" :key="index">
+            <td
+              v-for="(field, fieldIndex) in ticketTable.fields"
+              :key="fieldIndex"
+            >
+              {{ getTicketFieldValues(ticket, field) }}
+              <view-ticket :tickets="ticket"></view-ticket>
+            </td>
+            <td>
+              <v-chip :color="getColor(status)" flat small>{{
+                ticket.status
+              }}</v-chip>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+      <v-pagination
+        :length="length"
+        v-model="currentPage"
+        :per-page="perPage"
+        first-text="First"
+        prev-text="Prev"
+        pills
+        size="sm"
+        align="right"
+        class="pagination"
+      ></v-pagination>
     </v-card>
   </v-container>
 </template>
@@ -66,10 +74,11 @@ import { xpath_getter } from "../../utils/jsonHelpers";
 export default {
   data() {
     return {
-      perPage: 2,
+      perPage: 10,
       currentPage: 1,
       filter: "",
       props: ["items"],
+      search: "",
 
       tickets: [],
       ticketTable: {
@@ -149,16 +158,40 @@ export default {
     mounted() {
       this.retrieveTickets();
     },
+
+    getColor(status) {
+      if (status.value > "pending") return "red";
+      else if (status.value > open) return "green";
+      else return "grey";
+    },
   },
 };
 </script>
 
 <style scoped>
 .filter {
-  border: 1px solid rgb(205, 227, 245);
-  margin-left: 7%;
+  border: 1px solid #e0e0e0;
+  border-radius: 16px;
+  text-transform: capitalize;
+  box-sizing: border-box;
+}
+.pagination {
+  margin-left: 80%;
+  width: 207px;
+  height: 30px;
+  left: 1212px;
+  top: 847px;
+}
+.search {
+  border: 1px solid #e0e0e0;
   border-radius: 10px;
-  margin-bottom: 10px;
-  margin-top: 10px;
+  padding-left: 10px;
+  margin-right: 10px;
+  box-sizing: border-box;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+}
+.header {
+  margin-inline-start: 20px;
+  margin-top: 20px;
 }
 </style>
