b39bd1da54ac1da16f2f75929b0bd8518a2ab58f
kenneth4-pixel <akenakennethfareal@gmail.com>
2021-09-24 03:05:54 -0700

    deployment commit

--- src/main.js
+++ src/main.js
@@ -3,12 +3,12 @@ import vuetify from "./plugins/vuetify";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-import Vuelidate from "@vuelidate/validators";
+
 
 const app = createApp(App);
 app.use(router);
 app.use(store);
 app.use(vuetify);
-app.use(Vuelidate);
+
 
 app.mount("#app");
